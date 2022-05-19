ad85b3ab9ee62e233ab2cde80d950903934c241a
Karl-Otaner <carlosrnsilva01@gmail.com>
2022-05-19 00:39:39 -0300

    Criando nova activity Formulario aluno

--- app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,12 +1,8 @@
-package alura.com.br.agenda;
+package alura.com.br.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,16 +11,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import alura.com.br.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Carlos", "Alex" ,"Fram", "Maria", "Ana", "Pedro"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listeview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1, alunos));
